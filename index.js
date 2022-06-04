8b5adfee0705012dfae6da40cd5340e790b64899
caleb olusegun <olupeniel2000@gmail.com>
2022-06-04 23:49:20 +0100

    javascript logic for two breaker but not completed

---
+++ index.js
@@ -0,0 +1,6 @@
+const grid = document.querySelector('.box')
+const block = document.createElement('div');
+block.classList.add('small-box')
+block.style.left = '100px'
+block.style.bottom = '50px'
+grid.appendChild(small-box)
